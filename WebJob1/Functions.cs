48e4dba550617aad3924ec01cf51c75b75477257
Lino3D <kamiskijakub@gmail.com>
2017-06-03 21:57:32 +0200

    Queue based instead of Blob Based.

--- WebJob1/Functions.cs
+++ WebJob1/Functions.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Azure.WebJobs;
-using System.IO;
 
 namespace PositionJob
 {
@@ -7,19 +6,19 @@ namespace PositionJob
     {
         // This function will get triggered/executed when a new message is written 
         // on an Azure Queue called queue.
-        public static void ProcessBlobMessage([BlobTrigger("gismaincontainer/posdatarequest{name}")] TextReader message, TextWriter log)
-        {
-            log.WriteLine(message.ReadToEnd());
-            log.WriteLine("Test");
-        }
-        public static void CopyBlob([BlobTrigger("gismaincontainer/posdatarequest{name}")] TextReader input,
-            [Blob("mycontainer/response-{name}")] out string output)
-        {
-            output = input.ReadToEnd() + "LOL";
-        }
+        //public static void ProcessBlobMessage([BlobTrigger("gismaincontainer/posdatarequest{name}")] TextReader message, TextWriter log)
+        //{
+        //    log.WriteLine(message.ReadToEnd());
+        //    log.WriteLine("Test");
+        //}
+        //public static void CopyBlob([BlobTrigger("gismaincontainer/posdatarequest{name}")] TextReader input,
+        //    [Blob("mycontainer/response-{name}")] out string output)
+        //{
+        //    output = input.ReadToEnd() + "LOL";
+        //}
 
         public static void ModifyQueueMessage([QueueTrigger("messagequeuemainuser")] string queueMessage,
-            [Queue("messagequeuemainuser")] out string outputQueueMessage)
+            [Queue("responsequeuemainuser")] out string outputQueueMessage)
         {
             outputQueueMessage = "Response";
         }
